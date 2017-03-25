006c25e8eec0dbfdaefdedcc057258fb6c1edb3d
drsaaron <dr_saaron@yahoo.com>
2017-03-25 08:54:33 -0500

    set encoding to be UTF.

--- src/main/java/com/blazartech/products/mail/impl/spring/config/MailerConfiguration.java
+++ src/main/java/com/blazartech/products/mail/impl/spring/config/MailerConfiguration.java
@@ -7,6 +7,7 @@ package com.blazartech.products.mail.impl.spring.config;
 
 import com.blazartech.products.crypto.BlazarCryptoFile;
 import java.util.Properties;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -20,6 +21,8 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
  */
 @Configuration
 public class MailerConfiguration {
+    
+    private static final Logger logger = Logger.getLogger(MailerConfiguration.class);
     
     @Autowired
     private BlazarCryptoFile cryptoFile;
@@ -39,15 +42,19 @@ public class MailerConfiguration {
 
     @Bean
     public JavaMailSender getMailSender() {
+        logger.info("configuring mailer....");
+        
         JavaMailSenderImpl sender = new JavaMailSenderImpl();
         sender.setHost(host);
         sender.setPort(port);
         sender.setUsername(userID);
         sender.setPassword(cryptoFile.getPassword(userID, resourceID));
+        sender.setDefaultEncoding("UTF-8");
         
         Properties mailProperties = new Properties();
         mailProperties.setProperty("mail.smtp.auth", "true");
         mailProperties.setProperty("mail.smtp.starttls.enable", "true");
+        mailProperties.setProperty("mail.mime.charset", "ISO-8859-1");
         sender.setJavaMailProperties(mailProperties);
         
         return sender;
